41874f964ae154dce8dcd2e39854274157875383
anvaka <anvaka@gmail.com>
2013-10-24 09:11:45 -0700

    Added default position

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ module.exports = {
   // that's it for now
 };
 
-function Body() {
+function Body(x, y) {
+  this.pos = new Vector2d(x, y);
   this.force = new Vector2d();
-  this.pos = new Vector2d();
   this.mass = 1;
 }
 
-function Vector2d() {
-  this.x = 0;
-  this.y = 0;
+function Vector2d(x, y) {
+  this.x = typeof x === 'number' ? x : 0;
+  this.y = typeof y === 'number' ? y : 0;
 }
--- test/duck.js
+++ test/duck.js
@@ -13,5 +13,13 @@ test('Vector has x and y', function(t) {
   var vector = new primitives.Vector2d();
   t.ok(typeof vector.x === 'number', 'Vector has x coordinates');
   t.ok(typeof vector.y === 'number', 'Vector has y coordinates');
+
+  var initialized = new primitives.Vector2d(1, 2);
+  t.equal(initialized.x, 1, 'Vector initialized imporperly');
+  t.equal(initialized.y, 2, 'Vector initialized imporperly');
+
+  var badInput = new primitives.Vector2d('hello world');
+  t.equal(badInput.x, 0, 'Vector should be resilient to bed input');
+  t.equal(badInput.y, 0, 'Vector should be resilient to bed input');
   t.end();
 });
